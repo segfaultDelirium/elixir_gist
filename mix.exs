6cc306d973258f09d121969e8281d4857c0753b7
Your Name <you@example.com>
2023-10-07 08:27:24 +0200

    add dialyzir and credo

--- mix.exs
+++ mix.exs
@@ -9,7 +9,8 @@ defmodule ElixirGist.MixProject do
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      dialyzer: [flags: ["-Wunmatched_returns", :error_handling, :underspecs]]
     ]
   end
 
@@ -50,7 +51,9 @@ defmodule ElixirGist.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.4.1", only: [:dev], runtime: false},
     ]
   end
 
